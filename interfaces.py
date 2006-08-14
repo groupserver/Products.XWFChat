71acff3ceac8f72295e51d7ead98b72afd9a35e8
Richard Waid <richard@iopen.net>
2006-08-14 23:16:44 +0000

    new marker interfaces
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFChat/trunk%401185

--- interfaces.py
+++ interfaces.py
@@ -1,3 +1,13 @@
 from zope.interface.interface import Interface
 class IFolder(Interface):
     pass
+
+# Marker Interface
+class IGSGroupFolder(Interface):
+    pass
+
+class IGSChat(Interface):
+    pass
+
+class IGroupFolder(Interface):
+    pass
